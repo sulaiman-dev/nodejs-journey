8d310e2bacfc969eaa9daaae93d3dab53093742f
sulaimanks <sulaiman@iplex.co>
2022-01-10 16:50:50 -0500

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+l// Globals - no windows !!!
+
+// __dirname - path to current directory
+// __filename - file name
+// require - functions to use modules
+// module - info about current module (file)
+// process - info about env where the program is being executed.
+
+console.log(__dirname);
+setInterval(() => {
+    console.log("hello world")
+}, 1000);
---
+++ app.js
@@ -0,0 +1,11 @@
+// Modules
+const john = "john";
+const peter = "peter";
+
+const sayHello = (name) => {
+    console.log(`Hello, ${name}`)
+}
+
+sayHello("Ali");
+sayHello(john);
+sayHello(peter);
